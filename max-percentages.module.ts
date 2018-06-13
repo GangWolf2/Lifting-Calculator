c805548ba2417d64c9f5b8d3cb711e95035a8879
GangWolf2 <32149417+GangWolf2@users.noreply.github.com>
2018-06-12 19:31:43 -0500

    First Draft of Programs
    First draft, not fully debugged and still in progress

---
+++ max-percentages.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { MaxPercentagesPage } from './max-percentages';
+
+@NgModule({
+  declarations: [
+    MaxPercentagesPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(MaxPercentagesPage),
+  ],
+})
+export class MaxPercentagesPageModule {}
