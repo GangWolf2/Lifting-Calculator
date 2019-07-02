387c2bae924bded13ff84c6013bb7a75c4f2895f
GangWolf2 <32149417+GangWolf2@users.noreply.github.com>
2019-07-02 14:39:38 -0500

    Add files via upload

--- weight-finder.ts
+++ weight-finder.ts
@@ -15,31 +15,72 @@ import { ResultsPage } from '../results/results';
 })
 
 
-var userWeight; 
-var barSelected;
-var fortyfive, thirtyfive, twentyfive, ten, five, two_five;
 
 export class WeightFinderPage {
-
-
+  
+  protected userWeight: number;
+  protected bar;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    
   }
 
-  setUserWeight(weight){
-    userWeight = weight;
+  
+
+  protected bartype = [
+    {name: 'Olympic Bar', weight: 45, img: "https://imgur.com/CdaOvGq.png", isChecked: true},
+    {name: 'Training Bar', weight: 15, img: "https://imgur.com/zZJdsOI.png",isChecked: false},
+    {name: 'Hex/Trap Bar', weight: 45, img: "https://imgur.com/vA77495.png", isChecked: false},
+    {name: 'Yoke Bar', weight: 65, img: "https://imgur.com/HPupOjv.png", isChecked: false},
+    {name: 'Swiss Bar', weight: 35, img: "https://imgur.com/2RgEPaO.png", isChecked: false}
+  ];
+
+  private barTypeHolder = [
+    {name: 'Holder', weight: 0, img:"https://imgur.com/2RgEPaO.png", isChecked: false}
+  ];
+
+  setUserInput(){
+    
+  }
+  setBar(bar){
+    this.bar = bar;
   }
 
-  setBarType(bar){
-    barSelected = bar;
+  setBarsFalse(bar){ //idea is that html will check which if a bar is true, send that bar to this method
+    this.bartype[bar].isChecked = true; //now must move this bar to top of array
+
+    this.barTypeHolder[0] = this.bartype[bar];
+    this.bartype[bar] = this.bartype[0];
+    this.bartype[0] = this.barTypeHolder[0];
+
+    for(var i = 1; i < this.bartype.length; i++){
+      this.bartype[i].isChecked = false;
+    }
+    //and this method will go through bar array and set all other bars false
+                      //based on most current checkbox marked true, but will also send selected 
+                      //checkbox to the front of the array
+   
+  }
+  
+  boxDisable(bar){
+    this.bartype[bar].isChecked = false;
   }
 
-  setPlates(fourfive, threefive, twofive, t3n, fiv3, two5){
+  boxEnable(bar){
+    this.bartype[bar].isChecked = true;
+  }
+
+  setUserWeight(userWeight){
+    
+    this.userWeight = userWeight;
 
   }
 
   loadResultsPage(){
-    this.navCtrl.push(ResultsPage);
+    this.navCtrl.push(ResultsPage, {
+      userWeight: this.userWeight,
+      barWeight: this.bar.weight
+    });
 
-  }
+  
 
 }
